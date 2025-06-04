c7f6cfda99f9a026ea15b26464d36698b8cc3848
Rodrimg723 <rodrimg723@gmail.com>
2025-06-04 17:55:32 +0200

    final commit

--- IntroBBDD.sql
+++ IntroBBDD.sql
@@ -1,18 +1,28 @@
-Ejercicio 1
+-- Ejercicio 1
+SELECT 
+    flight_id,
+    flight_no,
+    status,
+FROM flights 
+WHERE status = 'On Time’;
 
+-- Ejercicio 2
+SELECT *
+FROM bookings 
+WHERE total_amount >= 1000000;
 
+-- Ejercicio 3
+SELECT * 
+FROM aircraft_data;
 
-Ejercicio 2
+-- Ejercicio 4
+SELECT 
+    flight_id,
+    flight_no 
+FROM flights 
+WHERE aircraft_code = '733'
 
-
-
-Ejercicio 3
-
-
-
-
-Ejercicio 4
-
-
-
-Ejercicio 5
+-- Ejercicio 5
+SELECT * 
+FROM tickets 
+WHERE passenger_name LIKE 'IRINA%';
